fb8cf399529614c439940829f104e555c773ba29
Joao-Felisberto <me@me.com>
2022-12-19 14:47:08 +0000

    Dynamically changing code can be now executed

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import glob
 import importlib
 import json
 import logging
-import os
 
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
@@ -27,41 +26,44 @@ app = Flask(__name__)
 csrf = CSRFProtect()
 csrf.init_app(app)
 
-routes = {}
+
+class Main:
+    def __init__(self):
+        self.routes = {}
+
+    def load_controllers(self):
+        self.load_routes()
+
+        controller_files = [f for f in glob.glob("controllers/*.py") if not f.endswith("__.py")]
+        controller_functions = {}
+        for file in controller_files:
+            controller_file = f"controllers.{file.split('/')[-1].replace('.py', '')}"
+            module = importlib.import_module(controller_file)
+            for func in [m for m in dir(module) if callable(getattr(module, m))]:
+                controller_functions[func] = getattr(module, func)
+
+        for endpoint, route in self.routes.items():
+            if bool(route.get("dev", False)) and cfg["DEBUG"]:
+                print(endpoint, route, "lambda")
+                app.add_url_rule(endpoint, route["endpointName"],
+                                 lambda _ep=route['endpointName']: (
+                                     logging.debug(f"running {_ep}(...)"),
+                                     self.load_routes(),
+                                     _res := (eval(self.routes[endpoint]["method"])),
+                                     f"<h1>Executed!</h1></br><p>{_res}</p>"
+                                 )[-1])
+            elif bool(route.get("dev", False)):
+                print(endpoint, route, "null")
+                app.add_url_rule(endpoint, route["endpointName"], lambda: None)
+            else:
+                print(endpoint, route, "method")
+                app.add_url_rule(endpoint, route["endpointName"], controller_functions[route["method"]])
+
+        return controller_functions
+
+    def load_routes(self):
+        with open("static/routes.json", 'r') as routesConfig:
+            self.routes = json.loads(routesConfig.read())
 
 
-def load_controllers():
-    with open("static/routes.json", 'r') as routesConfig:
-        routes = json.loads(routesConfig.read())
-
-    controller_files = [f for f in glob.glob("controllers/*.py") if not f.endswith("__.py")]
-    controller_functions = {}
-    for file in controller_files:
-        controller_file = f"controllers.{file.split('/')[-1].replace('.py', '')}"
-        module = importlib.import_module(controller_file)
-        for func in [m for m in dir(module) if callable(getattr(module, m))]:
-            controller_functions[func] = getattr(module, func)
-
-    for endpoint, route in routes.items():
-        if bool(route.get("dev", False)) and cfg["DEBUG"]:
-            print(endpoint, route, "lambda")
-            app.add_url_rule(endpoint, route["endpointName"],
-                             lambda: (logging.debug(f"running {route['endpointName']}(...)"),
-                                      exec(route["method"]),
-                                      "<h1>Executed!</h1>"
-                                      )[-1])
-        elif bool(route.get("dev", False)):
-            print(endpoint, route, "null")
-            app.add_url_rule(endpoint, route["endpointName"], lambda: None)
-        else:
-            print(endpoint, route, "method")
-            app.add_url_rule(endpoint, route["endpointName"], controller_functions[route["method"]])
-
-    return controller_functions
-
-
-# if __name__ == '__main__':
-print(os.getcwd())
-routes = load_controllers()
-
-# app.run()
+routes = Main().load_controllers()
